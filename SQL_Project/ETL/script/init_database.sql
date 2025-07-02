377d07964b748f96dcc06ab2ea32fd3ca6a40d19
Nadiia1998 <nadiakozorovska@gmail.com>
2025-07-02 16:46:08 +0200

    Update init_database.sql

--- SQL_Project/ETL/script/init_database.sql
+++ SQL_Project/ETL/script/init_database.sql
@@ -3,11 +3,11 @@
 Create Database and Schemas
 =============================================================
 Script Purpose:
-    This script creates a new database named 'HospitalWatehouseAnalysys' after checking if it already exists. 
+    This script creates a new database named 'HospitalWarehouseAnalysys' after checking if it already exists. 
     If the database exists, it is dropped and recreated. Additional its create table
 	
 WARNING:
-    Running this script will drop the entire 'HospitalWatehouseAnalysys' database if it exists. 
+    Running this script will drop the entire 'HospitalWarehouseAnalysys' database if it exists. 
     All data in the database will be permanently deleted. Proceed with caution 
     and ensure you have proper backups before running this script.
 */
@@ -15,19 +15,19 @@ WARNING:
 USE master;
 GO
 
-IF EXISTS (SELECT 1 FROM sys.databases WHERE name='HospitalWatehouseAnalysys')
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name='HospitalWarehouseAnalysys')
 BEGIN
-	ALTER DATABASE HospitalWatehouseAnalysys SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-	DROP DATABASE HospitalWatehouseAnalysys;
+	ALTER DATABASE HospitalWarehouseAnalysys SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE HospitalWarehouseAnalysys;
 END;
 GO
 
 
 
-CREATE DATABASE HospitalWatehouseAnalysys;
+CREATE DATABASE HospitalWarehouseAnalysys;
 GO
 
-USE HospitalWatehouseAnalysys;
+USE HospitalWarehouseAnalysys;
 GO
 
 CREATE SCHEMA bronze;
